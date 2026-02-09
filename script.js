db32808a7b9972a10c73f458e67cbc295b44627c
FilipW94 <filipwilhelmsson94@gmail.com>
2026-02-09 15:09:46 +0100

    DDE main page category is done

--- script.js
+++ script.js
@@ -19,3 +19,24 @@ function changeImage(thumbnail) {
 
     thumbnail.classList.add("active");
 }
+const bannerImages = [
+    "Images/DDE_Banner.png",
+    "Images/DDE_Banner2.png",
+    "Images/DDE_Banner3.png"
+];
+
+let currentBanner = 0;
+const bannerImg = document.getElementById("DDE_Banner");
+
+function showBanner(index) {
+    currentBanner = (index + bannerImages.length) % bannerImages.length; // wrap around
+    bannerImg.src = bannerImages[currentBanner];
+}
+
+function nextBanner() {
+    showBanner(currentBanner + 1);
+}
+
+function prevBanner() {
+    showBanner(currentBanner - 1);
+}
